d892a470004a3c4b5610a6809890a661f0635e09
gilberto asuaje <asuajegilberto@gmail.com>
2020-05-28 09:50:07 -0500

    soporte a webpack para router

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CompressionPlugin = require('compression-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 const javascriptRules = {
   test: /\.js$/,
   exclude: /node_modules/,
   use: {
-    loader: 'babel-loader',
+    loader: "babel-loader",
     options: {
-      presets: [
-        '@babel/preset-react',
-        '@babel/preset-env'
-      ],
-      plugins: ['@babel/plugin-proposal-optional-chaining']
-    }
-  }
-}
+      presets: ["@babel/preset-react", "@babel/preset-env"],
+      plugins: ["@babel/plugin-proposal-optional-chaining"],
+    },
+  },
+};
 
-const developmentPlugins = []
-const productionPlugins = [
-  new CompressionPlugin()
-]
+const developmentPlugins = [];
+const productionPlugins = [new CompressionPlugin()];
 
-module.exports = (env, {mode}) => ({
+module.exports = (env, { mode }) => ({
+  devServer: {
+    //contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
   output: {
-    filename: 'app.[contentHash].js'
+    filename: "app.[contentHash].js",
   },
   module: {
-    rules: [
-      javascriptRules
-    ]
+    rules: [javascriptRules],
   },
   plugins: [
-    ...(mode === 'production' ? productionPlugins : developmentPlugins),
+    ...(mode === "production" ? productionPlugins : developmentPlugins),
     new HtmlWebpackPlugin({
-      title: 'Webpack Paso a Paso',
-      template: 'src/index.html'
-    })
-  ].filter(Boolean)
-})
+      title: "Webpack Paso a Paso",
+      template: "src/index.html",
+    }),
+  ].filter(Boolean),
+});
